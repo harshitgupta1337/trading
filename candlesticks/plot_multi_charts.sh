13908d8002bc0344bcdf18f831f3f6695124590d
Harshit Gupta <guptaharshit@microsoft.com>
2023-02-09 22:00:35 -0800

    Make plots and data dirs; Fix path of plot_candlesticks.py

--- candlesticks/plot_multi_charts.sh
+++ candlesticks/plot_multi_charts.sh
@@ -2,13 +2,16 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/../venv/bin/activate
 
+mkdir -p ./plots
+mkdir -p ./data
+
 tickers_file=$1
 
 pids=""
 for ticker in $(cat $tickers_file)
 do
     echo "Launching plotting for ticker $ticker"
-    python3 plot_candlesticks.py -O ./plots/ -T $ticker > /dev/null #&
+    python3 $SCRIPT_DIR/plot_candlesticks.py -O ./plots/ -T $ticker > /dev/null #&
     #pids="$pids $!"
 done
 for pid in $pids
