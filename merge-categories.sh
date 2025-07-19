2861974504ed3764a981f81aaf0fa07367653ed5
Harshit Gupta <harshitgupta1337@gmail.com>
2025-07-18 22:46:49 -0400

    Create plots/categories dir

--- merge-categories.sh
+++ merge-categories.sh
@@ -5,6 +5,8 @@ source ./venv/bin/activate
 # Get all the categories
 categories=$(ls ./tickers/*)
 
+mkdir -p ./plots/categories
+
 for category in $categories; do
   ./merge-pdfs.sh $(basename $category)
 done
